ed3a60fd131c57aad258c273ea8f22deaca598bc
Andrea <Andrea@AndreaGran>
2015-10-21 08:42:40 -0300

    Página de Login

---
+++ metadatos/tablas/apex_pagina_tipo.sql
@@ -0,0 +1,16 @@
+
+------------------------------------------------------------
+-- apex_pagina_tipo
+------------------------------------------------------------
+INSERT INTO apex_pagina_tipo (proyecto, pagina_tipo, descripcion, clase_nombre, clase_archivo, include_arriba, include_abajo, exclusivo_toba, contexto, punto_montaje) VALUES (
+	'designa', --proyecto
+	'designa', --pagina_tipo
+	'Página de Login SDD', --descripcion
+	'tp_designa', --clase_nombre
+	'tp_designa.php', --clase_archivo
+	NULL, --include_arriba
+	NULL, --include_abajo
+	NULL, --exclusivo_toba
+	NULL, --contexto
+	'23'  --punto_montaje
+);
