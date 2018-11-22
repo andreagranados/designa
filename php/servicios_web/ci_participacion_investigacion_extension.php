32c39e008dca76f08bd9a8e1b741a546e7255a6f
Andrea <Andrea@AndreaGran>
2018-11-22 10:05:42 -0300

    Cambia la consulta del servicio web

--- php/servicios_web/ci_participacion_investigacion_extension.php
+++ php/servicios_web/ci_participacion_investigacion_extension.php
@@ -4,14 +4,14 @@ class ci_participacion_investigacion_extension extends toba_ci
 	function ini()
 	{
             //$sql="select apellido,nro_docum from persona";
-            $sql="select * from (select 'I' as tipo,i.denominacion,rescd as norma,doc.nro_docum,f.descripcion as funcion,pi.carga_horaria,pi.desde,pi.hasta "
+            $sql="select * from (select 'I' as tipo,i.denominacion,i.nro_resol as norma,doc.nro_docum,f.descripcion as funcion,pi.carga_horaria,pi.desde,pi.hasta "
                     . " from integrante_interno_pi pi "
                     . " inner join pinvestigacion i on ( pi.pinvest=i.id_pinv ) "
                     . " inner join designacion d on (d.id_designacion=pi.id_designacion)  "
                     . " inner join docente doc on (d.id_docente=doc.id_docente)"
                     . " inner join funcion_investigador f on (f.id_funcion=pi.funcion_p)"
                     . " UNION "
-                    ."select 'I' as tipo,i.denominacion,rescd as norma,p.nro_docum,f.descripcion as funcion,pi.carga_horaria,pi.desde,pi.hasta "
+                    ."select 'I' as tipo,i.denominacion,i.nro_resol as norma,p.nro_docum,f.descripcion as funcion,pi.carga_horaria,pi.desde,pi.hasta "
                     . " from integrante_externo_pi pi "
                     . " inner join pinvestigacion i on ( pi.pinvest=i.id_pinv ) "
                     . " inner join persona p on (p.tipo_docum=pi.tipo_docum and p.nro_docum=pi.nro_docum)  "
