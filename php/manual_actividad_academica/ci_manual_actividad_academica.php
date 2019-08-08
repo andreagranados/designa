860d7bafae12ee9b2836ba4ef8df290542800387
Andrea <Andrea@AndreaGran>
2019-08-08 07:14:46 -0300

    Cambio Instructivo Investigacion

--- php/manual_actividad_academica/ci_manual_actividad_academica.php
+++ php/manual_actividad_academica/ci_manual_actividad_academica.php
@@ -2,7 +2,7 @@
 /*buscar el archivo pdf y descargarlo */
 header("Content-type:application/pdf");
 // It will be called downloaded.pdf
-header("Content-Disposition:attachment;filename='Manual_SDD_AA.pdf'");
+header("Content-Disposition:attachment;filename=Manual_SDD_AA.pdf");
 // The PDF source is in original.pdf
 readfile("./Manual_SDD_AA.pdf");
 ?>
--- php/manual_circuito_presupuesto/ci_manual_circuito_presupuesto.php
+++ php/manual_circuito_presupuesto/ci_manual_circuito_presupuesto.php
@@ -2,7 +2,7 @@
 /*buscar el archivo pdf y descargarlo */
 header("Content-type:application/pdf");
 // It will be called downloaded.pdf
-header("Content-Disposition:attachment;filename='ManualModuloDesignaciones.pdf'");
+header("Content-Disposition:attachment;filename=ManualModuloDesignaciones.pdf");
 // The PDF source is in original.pdf
 readfile("./ManualModuloDesignaciones.pdf");
 ?>
