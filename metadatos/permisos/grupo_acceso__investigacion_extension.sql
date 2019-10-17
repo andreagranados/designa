b92bbc4ae6ce56b103b89f43f6ddce1efa480b02
Andrea <Andrea@AndreaGran>
2019-10-17 08:50:16 -0300

    Vuelvo a crear Perfil Investigacion y Extension (por error lo elimine en el commit anterior)

---
+++ metadatos/permisos/grupo_acceso__investigacion_extension.sql
@@ -0,0 +1,248 @@
+
+------------------------------------------------------------
+-- apex_usuario_grupo_acc
+------------------------------------------------------------
+INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_acceso, descripcion, vencimiento, dias, hora_entrada, hora_salida, listar, permite_edicion, menu_usuario) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'Investigación y Extensión', --nombre
+	NULL, --nivel_acceso
+	'Para Secretaria de Investigacion y Secretaria Ext', --descripcion
+	NULL, --vencimiento
+	NULL, --dias
+	NULL, --hora_entrada
+	NULL, --hora_salida
+	NULL, --listar
+	'0', --permite_edicion
+	NULL  --menu_usuario
+);
+
+------------------------------------------------------------
+-- apex_usuario_grupo_acc_item
+------------------------------------------------------------
+
+--- INICIO Grupo de desarrollo 0
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'1'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'2'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3635'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3636'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3652'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3685'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3686'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3687'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3689'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3692'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3705'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3719'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3721'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3728'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3734'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3735'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3736'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3737'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3738'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3739'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3743'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3755'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3757'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3759'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3765'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3772'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3773'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3781'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	NULL, --item_id
+	'3784'  --item
+);
+--- FIN Grupo de desarrollo 0
+
+------------------------------------------------------------
+-- apex_grupo_acc_restriccion_funcional
+------------------------------------------------------------
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'4'  --restriccion_funcional
+);
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'13'  --restriccion_funcional
+);
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'15'  --restriccion_funcional
+);
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'19'  --restriccion_funcional
+);
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'25'  --restriccion_funcional
+);
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'33'  --restriccion_funcional
+);
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'41'  --restriccion_funcional
+);
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'42'  --restriccion_funcional
+);
+INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
+	'designa', --proyecto
+	'investigacion_extension', --usuario_grupo_acc
+	'44'  --restriccion_funcional
+);
