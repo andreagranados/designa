2a3f29c5491c8b273d7b2b45f5f230e0142fa4f3
andrea <andrea@andrea-desktop>
2020-02-20 10:10:30 -0300

    Se agrega filtro legajo en Equipos de Catedra Se agrega campos celular y telefono en tabla docente (la actualizacion de datos personales actualiza tambien estos campos trayendolos de SIU-Mapuche) Se agrega la columna DNI (opcional) en reporte Informe de Actividad

--- metadatos/componentes/toba_ci/dump_4058.sql
+++ metadatos/componentes/toba_ci/dump_4058.sql
@@ -114,6 +114,18 @@ INSERT INTO apex_objeto_mt_me (objeto_mt_me_proyecto, objeto_mt_me, ev_procesar_
 --- INICIO Grupo de desarrollo 0
 INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
 	'designa', --proyecto
+	'3245', --dep_id
+	'4058', --objeto_consumidor
+	'4399', --objeto_proveedor
+	'columnas', --identificador
+	NULL, --parametros_a
+	NULL, --parametros_b
+	NULL, --parametros_c
+	NULL, --inicializar
+	NULL  --orden
+);
+INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
+	'designa', --proyecto
 	'2908', --dep_id
 	'4058', --objeto_consumidor
 	'4060', --objeto_proveedor
@@ -192,8 +204,15 @@ INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id)
 	'designa', --proyecto
 	'1674', --pantalla
 	'4058', --objeto_ci
-	'1', --orden
+	'2', --orden
 	'2908'  --dep_id
+);
+INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
+	'designa', --proyecto
+	'1674', --pantalla
+	'4058', --objeto_ci
+	'1', --orden
+	'3245'  --dep_id
 );
 
 ------------------------------------------------------------
