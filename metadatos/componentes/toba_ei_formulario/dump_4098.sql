30fe29c2823d1ca99878acfd2a2e5962bdef89b6
Andrea <Andrea@LAPTOP-CK2T37FA>
2021-06-02 13:41:54 -0300

    Solo lectura campos categ y dedicacion estatuto dentro del formulario de alta reservas.

--- metadatos/componentes/toba_ei_formulario/dump_4098.sql
+++ metadatos/componentes/toba_ei_formulario/dump_4098.sql
@@ -651,7 +651,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	'0', --permitir_html
 	'0', --deshabilitar_rest_func
 	NULL, --estado_defecto
-	'0', --solo_lectura
+	'1', --solo_lectura
 	'0', --solo_lectura_modificacion
 	'get_categ_estatuto', --carga_metodo
 	NULL, --carga_clase
@@ -734,7 +734,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	'0', --permitir_html
 	'0', --deshabilitar_rest_func
 	NULL, --estado_defecto
-	'0', --solo_lectura
+	'1', --solo_lectura
 	'0', --solo_lectura_modificacion
 	'get_dedicacion_categoria', --carga_metodo
 	NULL, --carga_clase
