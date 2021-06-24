b32acaab8133446ebd3f12d393f7d6f953be9ee8
Andrea <Andrea@LAPTOP-CK2T37FA>
2021-06-24 09:31:53 -0300

    Formulario de alta de reservas, solo dejo disponible caracter R, I.

--- metadatos/componentes/toba_ei_formulario/dump_4098.sql
+++ metadatos/componentes/toba_ei_formulario/dump_4098.sql
@@ -823,7 +823,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	NULL, --carga_consulta_php
 	NULL, --carga_sql
 	'designa', --carga_fuente
-	'R,I,O,S', --carga_lista
+	'R,I', --carga_lista
 	NULL, --carga_col_clave
 	NULL, --carga_col_desc
 	NULL, --carga_maestros
