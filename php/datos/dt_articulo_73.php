21a9611dc732d704a329611ea591470e185413ad
Andrea <Andrea@LAPTOP-CK2T37FA>
2021-09-15 10:58:05 -0300

    Condicion desplegable designaciones en Art73

--- php/datos/dt_articulo_73.php
+++ php/datos/dt_articulo_73.php
@@ -197,7 +197,7 @@ class dt_articulo_73 extends designa_datos_tabla
                                     and carac='I'
                                     and ((c.cat_estat=b.cat_estat and c.desde<='2018-03-23') 
                                           or 
-                                          (c.cat_estat='PAD' and c.desde<='2019-09-23' ))
+                                          (c.cat_estat='PAD' and c.desde<='2019-09-23' and concursado=1))
                                      )";
                     
             $legajos=toba::db('designa')->consultar($sql);
@@ -244,7 +244,7 @@ class dt_articulo_73 extends designa_datos_tabla
                                     and carac='I'
                                     and ((c.cat_estat=b.cat_estat and c.desde<='2018-03-23' )
                                          or 
-                                         (c.cat_estat='PAD' and c.desde<='2019-09-23'))
+                                         (c.cat_estat='PAD' and c.desde<='2019-09-23' and concursado=1))
                                      ) "
                        .$concatenar
                       
